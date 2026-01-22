5ec38fcb451aa98bf38328da8b6c07592e9add5a
dawidgdowik1 <gdowikdawid38@gmail.com>
2026-01-22 12:48:56 +0100

    Dodanie klasy DatabaseService i test połączenia z bazą danych

---
+++ Database/DatabaseService.cs
@@ -0,0 +1,47 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Data;
+
+namespace PROJEKT_72413.Database
+{
+    public class DatabaseService
+    {
+        private string connectionString = "server=localhost;database=biuro_podrozy;uid=root;pwd=admin123;";
+
+        public bool TestConnection()
+        {
+            try
+            {
+                using (MySqlConnection conn = new MySqlConnection(connectionString))
+                {
+                    conn.Open();
+                    return true;
+                }
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public DataTable GetTable(string query)
+        {
+            DataTable dt = new DataTable();
+            try
+            {
+                using (MySqlConnection conn = new MySqlConnection(connectionString))
+                {
+                    conn.Open();
+                    MySqlCommand cmd = new MySqlCommand(query, conn);
+                    MySqlDataAdapter adapter = new MySqlDataAdapter(cmd);
+                    adapter.Fill(dt);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            return dt;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using PROJEKT_72413.Database;
+
+namespace PROJEKT_72413
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DatabaseService db = new DatabaseService();
+
+            if (db.TestConnection())
+            {
+                Console.WriteLine("Sukces: Polaczono z baza");
+            }
+            else
+            {
+                Console.WriteLine("Blad: Brak polaczenia");
+            }
+
+            Console.WriteLine("Nacisnij dowolny klawisz...");
+            Console.ReadKey();
+        }
+    }
+}
